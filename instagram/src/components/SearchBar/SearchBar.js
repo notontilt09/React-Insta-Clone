0376c9d90c5a4aca746c3091f576059e5f44111a
Daniel Weinman <namniewd@gmail.com>
2019-01-17 16:33:40 -0500

    updated SearchBar.js to only use styled-components

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,14 +1,57 @@
 import React from 'react'
 import './SearchBar.css'
+import styled from 'styled-components';
+
+const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+    margin: 20px 0;
+    padding-bottom: 30px;
+    border-bottom: 1px solid lightgray;
+`;
+
+const Left = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 25%;
+`;
+
+const InstaLogo = styled.img`
+    width: 150px;
+    height: auto;
+    border-left: 1px solid black;
+`;
+
+const Center = styled.div `
+    text-align: center;
+
+    input {
+        background: rgb(243, 243, 243);
+
+        &:: placeholder {
+            text-align: center;
+        }
+    }
+`;
+
+const Right = styled.div `
+    display: flex;
+    justify-content: space-evenly;
+    width: 30%;
+`;
 
 const SearchBar = props => {
     return (
-        <div className="searchBar">
-            <div className="left">
+        // <div className="searchBar">
+        <Header>
+            <Left>
                 <i className="fab fa-instagram fa-3x"></i>
-                <img className='insta-logo' src='https://fontmeme.com/images/instagram-new-logo.png' alt="insta" />
-            </div>
-            <div className='center'>
+                <InstaLogo src='https://fontmeme.com/images/instagram-new-logo.png' alt="insta" />
+            </Left>
+            <Center>
                 <form className="searchForm">
                     <input 
                         className="searchBox" 
@@ -18,13 +61,14 @@ const SearchBar = props => {
                         value={props.searchText}
                     />
                 </form>
-            </div>
-            <div className="right">
+            </Center>
+            <Right>
                 <i className="far fa-compass fa-2x"></i>
                 <i className="far fa-heart fa-2x"></i>
                 <i onClick={props.logout} className="far fa-user fa-2x"></i>
-            </div>
-        </div>
+            </Right>
+        </Header>
+        // </div>
     )
 };
 
